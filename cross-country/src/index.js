9d2a799df7e3d1cbb5b33b50dac09395d23f3f59
VictoriaCL08 <colonlav@email.sc.edu>
2025-03-27 08:43:03 -0400

    react site

--- cross-country/src/index.js
+++ cross-country/src/index.js
@@ -11,7 +11,7 @@ import Nopage from './pages/Nopage';
 
 function App(){
   return(
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
