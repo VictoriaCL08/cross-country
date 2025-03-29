f00de17f8f6c306e2087b68ada7a656dd813df6d
VictoriaCL08 <colonlav@email.sc.edu>
2025-03-29 16:51:55 -0400

    records page

--- xc/src/Layout.jsx
+++ xc/src/Layout.jsx
@@ -1,13 +1,13 @@
 import {Outlet} from "react-router-dom";
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Content from './components/ContentGarnetL';
 
 function Layout() {
     return (
         <>
             <Header />
             <Outlet />
-            <Footer />
         </>
     );
 };
--- xc/src/pages/ClubRecords.jsx
+++ xc/src/pages/ClubRecords.jsx
@@ -3,7 +3,179 @@ import './css/ClubRecords.css'
 function ClubRecords() {
   return (
     <main>
-    <h1>Club Records Page</h1>
+    <h2>Women's Records</h2>
+      <table class="record-table">
+          <tr>
+              <th>Event</th>
+              <th>Name</th>
+              <th>Mark</th>
+              <th>Meet</th>
+              <th>Year</th>
+          </tr>
+          <tr>
+              <td>5K XC</td>
+              <td>Victoria Colon-LaBorde</td>
+              <td>15:42</td>
+              <td>McAlpine</td>
+              <td>2023</td>
+          </tr>
+          <tr>
+              <td>6K XC</td>
+              <td>Carina Burdick</td>
+              <td>19:27</td>
+              <td>NIRCA Regionals</td>
+              <td>2024</td>
+          </tr>
+          <tr>
+              <td>5000 m</td>
+              <td>Wilhemina Cuono</td>
+              <td>17:37</td>
+              <td>Gamecock Opener</td>
+              <td>2017</td>
+          </tr>
+          <tr>
+              <td>3000 m</td>
+              <td>Katy Hall</td>
+              <td>8:26</td>
+              <td>UNC Charlotte</td>
+              <td>2021</td>
+          </tr>
+          <tr>
+              <td>1600 m</td>
+              <td>Claire McGarvey</td>
+              <td>5:05</td>
+              <td>Gamecock Opener</td>
+              <td>2022</td>
+          </tr>
+          <tr>
+              <td>800 m</td>
+              <td>Ellie Toth</td>
+              <td>2:12</td>
+              <td>NIRCA Regionals</td>
+              <td>2019</td>
+          </tr>
+          <tr>
+              <td>400 m</td>
+              <td>Abby Sonderfan</td>
+              <td>0:51.26</td>
+              <td>Garnet Grand Prix</td>
+              <td>2023</td>
+          </tr>
+          <tr>
+              <td>200 m</td>
+              <td>Meridith Moyer</td>
+              <td>0:20.34</td>
+              <td>Mountaineer Track Meet</td>
+              <td>2017</td>
+          </tr>
+          <tr>
+              <td>100 m</td>
+              <td>Carina Burdick</td>
+              <td>0:01.29</td>
+              <td>USC Opener</td>
+              <td>2019</td>
+          </tr>
+          <tr>
+              <td>High Jump</td>
+              <td>Sydney Rose</td>
+              <td>15' 3"</td>
+              <td>Anderson Track Meet</td>
+              <td>2023</td>
+          </tr>
+      </table>
+
+      <h2>Men's Records</h2>
+      <table class="record-table">
+          <tr>
+              <th>Event</th>
+              <th>Name</th>
+              <th>Mark</th>
+              <th>Meet</th>
+              <th>Year</th>
+          </tr>
+          <tr>
+              <td>5K XC</td>
+              <td>Patrick Champion</td>
+              <td>36:53</td>
+              <td>Mountaineer Invitational</td>
+              <td>2024</td>
+          </tr>
+          <tr>
+              <td>8K XC</td>
+              <td>Brady Pinkerman</td>
+              <td>26:26</td>
+              <td>Mountaineer Invite</td>
+              <td>2026</td>
+          </tr>
+          <tr>
+              <td>5000 m</td>
+              <td>Rylan Dodds</td>
+              <td>21:17</td>
+              <td>McAlpine</td>
+              <td>2021</td>
+          </tr>
+          <tr>
+              <td>3000 m</td>
+              <td>Michael Davis</td>
+              <td>12:31</td>
+              <td>McAlpine Track Meet</td>
+              <td>2021</td>
+          </tr>
+          <tr>
+              <td>1600 m</td>
+              <td>Christian Geils</td>
+              <td>12:42</td>
+              <td>Gamecock Opener</td>
+              <td>2023</td>
+          </tr>
+          <tr>
+              <td>800 m</td>
+              <td>Ryan Dixon</td>
+              <td>2:37.43</td>
+              <td>UNC Chapel Hill Meet</td>
+              <td>2025</td>
+          </tr>
+          <tr>
+              <td>400 m</td>
+              <td>Dylan Bent</td>
+              <td>0:38.43</td>
+              <td>USC Opener</td>
+              <td>2025</td>
+          </tr>
+          <tr>
+              <td>200 m</td>
+              <td>Ryan Young</td>
+              <td>0:42.32</td>
+              <td>Georgia Track Meet</td>
+              <td>2023</td>
+          </tr>
+          <tr>
+              <td>100 m</td>
+              <td>Jack Wegmet</td>
+              <td>0:12.32</td>
+              <td>Georgia Track Meet</td>
+              <td>2014</td>
+          </tr>
+      </table>
+
+      <h2>4x4x48 Hall of Fame</h2>
+      <table class="record-table">
+          <tr>
+              <th colspan = "2">These individuals completed the 4x4x48 4 times</th>
+          </tr>
+          <tr>
+              <th>Name</th>
+              <th>Years Completed</th>
+          </tr>
+          <tr>
+              <td>Katy Hall</td>
+              <td>2021-2024</td>
+          </tr>
+          <tr>
+              <td>Jack Wegmet</td>
+              <td>2020-2023</td>
+          </tr>   
+      </table>
     </main>
   )  
 };
