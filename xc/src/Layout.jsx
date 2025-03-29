a0de8530364a551340e735832930e3f9044e6d59
VictoriaCL08 <colonlav@email.sc.edu>
2025-03-29 18:19:07 -0400

    part 8

--- xc/src/Layout.jsx
+++ xc/src/Layout.jsx
@@ -1,7 +1,5 @@
 import {Outlet} from "react-router-dom";
 import Header from './components/Header';
-import Footer from './components/Footer';
-import Content from './components/ContentGarnetL';
 
 function Layout() {
     return (
