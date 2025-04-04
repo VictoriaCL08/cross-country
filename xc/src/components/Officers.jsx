9c735ebc2033bacdfb631f542c28f061060a46c1
VictoriaCL08 <colonlav@email.sc.edu>
2025-04-04 15:32:00 -0400

    is render working

--- xc/src/components/Officers.jsx
+++ xc/src/components/Officers.jsx
@@ -9,7 +9,7 @@ const Officers = () => {
 
     useEffect(()=>{
         (async ()=>{
-            const response = await axios.get("http://localhost:3001/api/officers");
+            const response = await axios.get("https://xc-server-backend.onrender.com/api/officers");
             setofficers(response.data);
         })();
 
