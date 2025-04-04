351e5507fe442ab06d81d8b0d3db52b7809c7f81
VictoriaCL08 <colonlav@email.sc.edu>
2025-04-04 15:53:03 -0400

    Project part 09

--- xc/src/components/Event.jsx
+++ xc/src/components/Event.jsx
@@ -5,7 +5,7 @@ function Event(prop) {
     <div className="event">
         <h3>{prop.name}</h3>
         <p>{prop.date}</p>
-        <img src={"https://xc-server-backend.onrender.com/" + prop.img}/>
+        <img src={"https://xc-server-backend.onrender.com/"+prop.img}/>
     </div>
   )  
 };
